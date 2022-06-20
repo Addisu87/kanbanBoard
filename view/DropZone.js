e891c8b4ff36c12a041de1b3a1de75b828add00e
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-06-20 09:42:41 +0300

    Fix linter errors

--- view/DropZone.js
+++ view/DropZone.js
@@ -7,8 +7,7 @@ export default class DropZone {
     range.selectNode(document.body);
 
     const dropZone = range.createContextualFragment(
-      `<div class="kanban__dropzone"></div>
-      `,
+      '<div class="kanban__dropzone"></div>',
     ).children[0];
 
     dropZone.addEventListener('dragover', (e) => {
